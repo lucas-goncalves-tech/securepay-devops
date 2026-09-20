fd706e9ee875fb96dbddcf503a69351ae4664ce1
Drummonds <drummond05@proton.me>
2026-09-19 21:45:14 -0300

    feat: devops tutor skills

---
+++ infra/vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.10.0/24"
+}
+resource "aws_subnet" "db" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.20.0/24"
+}
+
